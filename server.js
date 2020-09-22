9f94052d6dc27cfcbb07ba359474ce9da2e00e99
Andres A <acosta.andres.r@gmail.com>
2020-09-22 13:28:05 -0500

    // Set up static assets.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 // ==============================================================================
 
 var express = require("express");
+var path = require("path");
+
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -19,6 +21,9 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Set up static assets
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ================================================================================
 // ROUTER
